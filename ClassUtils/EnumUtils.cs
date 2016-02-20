33cea016c1560d3f932128c301cf0d1981000820
Mohammed Header <mohheader@hotmail.com>
2016-02-20 11:21:49 +0200

    Fix conflict with other EnumUtils class

--- ClassUtils/EnumUtils.cs
+++ ClassUtils/EnumUtils.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Linq;
 
-public static class EnumUtils {
+namespace Common{
+	public static class EnumUtils {
 
-	public static T Random<T> ()
-	{
-		var v = System.Enum.GetValues (typeof (T));
-		return (T) v.GetValue (new System.Random ().Next(v.Length));
+		public static T Random<T> ()
+		{
+			var v = System.Enum.GetValues (typeof (T));
+			return (T) v.GetValue (new System.Random ().Next(v.Length));
+		}
 	}
 }
