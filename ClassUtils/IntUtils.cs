f04066360b9254db6d834f6cf8784c5f78d00cb2
Mohammed Header <mohheader@hotmail.com>
2016-04-05 14:05:35 +0200

    Add some Utils

---
+++ ClassUtils/IntUtils.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class IntUtils {
+	public static void Times(this int count, System.Action action)
+	{
+		for (int i = 0; i < count; i++)
+		{
+			action();
+		}
+	}
+}
---
+++ Utils/ColorUtils/ColorUtils.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorUtils : MonoBehaviour {
+	static Color[] colors = new Color[]{Color.blue, Color.red, Color.green, Color.yellow};
+
+	public static Color Random(){
+		return colors[UnityEngine.Random.Range(0, colors.Length)];
+	}
+}
